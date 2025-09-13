0d020d8d8fdad3771b1a42041c46c3fe397bc034
Rasyahodhe <rasyahodhe450@gmail.com>
2025-09-13 17:47:43 +0800

    Menambahkan icon

---
+++ src/data/linktree.ts
@@ -0,0 +1,28 @@
+export const dataLinktree = {
+  header: {
+    title: "Linktree",
+    description: "Linktree",
+  },
+  linktree: [
+    {
+      name: "Instagram",
+      link: "https://www.google.com",
+      image: "",
+    },
+    {
+      name: "Facebook Fan Base Kendari",
+      link: "https://www.google.com",
+      image: "",
+    },
+    {
+      name: "Facebook Fan Base Kendari",
+      link: "https://www.google.com",
+      image: "",
+    },
+    {
+      name: "Facebook Fan Base Bau-Bau",
+      link: "https://www.google.com",
+      image: "",
+    },
+  ],
+};
